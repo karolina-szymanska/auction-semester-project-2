503f20826b47c9387df40568f0ab5494d4739820
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-16 18:58:16 +0200

    writing function displaying all listings in progress

--- src/scripts/listings-page/displayListingsUnlogged.js
+++ src/scripts/listings-page/displayListingsUnlogged.js
@@ -1,9 +1,37 @@
-// import { allActiveListingsDesc } from "../api/api-urls;";
+import { allActiveListingsDesc } from "../api/api-urls.js";
 import setLoader from "../components/loader.js";
 const listingsContainer = document.querySelector("#listings-container");
 
-function displayListingsUnlogged() {
+async function displayListingsUnlogged(url) {
   listingsContainer.innerHTML = setLoader();
+
+  try {
+    const response = await fetch(url);
+    const results = await response.json();
+    console.log(results);
+
+    for (let i = 0; i < results; i++) {
+      listingsContainer.innerHTML += `
+        <div class="col-12 col-md-6 col-lg-3">
+            <div class="card">
+                <div class="card-header">Offer number: 123456</div>
+                <div class="ratio ratio-4x3">
+                    <img src="${results[i].media[0]}" class="card-img-center" alt="${results[i].title}"/>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${results[i].title}</h5>
+                    <p class="card-text">Bids: ${results[i].bids.length}</p>
+                </div>
+            </div>
+        </div>`;
+    }
+
+    // if(results[i].media.length ===0) {
+    //     results[i].media = ["/assets/placeholders/placeholder.jpg"];
+    // };
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-displayListingsUnlogged();
+displayListingsUnlogged(allActiveListingsDesc);
