c155138bd2a454a255a28b0681de042205daaf8e
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-16 18:56:32 +0200

    changed  ""   into ``    in urls

--- src/scripts/api/api-urls.js
+++ src/scripts/api/api-urls.js
@@ -1,6 +1,5 @@
 // base url
-export const api_base_url = "https://api.noroff.dev/api/v1";
+export const api_base_url = `https://api.noroff.dev/api/v1`;
 
 // all active listings descending, including created by a registered user
-export const allActiveListingsDesc =
-  "${api_base_url}/auction/listings?sort=created&sortOrder=desc&_active=true";
+export const allActiveListingsDesc = `${api_base_url}/auction/listings?sort=created&sortOrder=desc&_active=true`;
