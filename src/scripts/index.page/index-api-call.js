597c725a40f9e1600207b94254792f9c04b143ba
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-18 00:59:26 +0200

    added 1 import + finally block

--- src/scripts/index.page/index-api-call.js
+++ src/scripts/index.page/index-api-call.js
@@ -1,15 +1,17 @@
 import { latestListings } from "../api/api-urls.js";
 import { displayListingsUnlogged } from "../components/displayListingsUnlogged.js";
+import { searchListings } from "../components/search.js";
 
 async function getUnloggedListings(url) {
   try {
     const response = await fetch(url);
     const listings = await response.json();
     console.log(listings);
-
     displayListingsUnlogged(listings);
   } catch (error) {
     console.log(error);
+  } finally {
+    searchListings();
   }
 }
 
