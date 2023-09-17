b8382e66da9aaa0159fa18721550b0a98d55cc15
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-17 02:14:05 +0200

    added some styles

--- src/scripts/api/api-urls.js
+++ src/scripts/api/api-urls.js
@@ -1,5 +1,8 @@
 // base url
 export const api_base_url = `https://api.noroff.dev/api/v1`;
 
+// 4 latest listings from the index page, including created by a registered user
+export const latestListings = `${api_base_url}/auction/listings?sort=created&sortOrder=desc&limit=4&_active=true`;
+
 // all active listings descending, including created by a registered user
 export const allActiveListingsDesc = `${api_base_url}/auction/listings?sort=created&sortOrder=desc&_active=true`;
