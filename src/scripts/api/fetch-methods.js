4b528ed74291a2efb15baed3ac77c59b8960f8c6
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-18 22:58:04 +0200

    added all necessary fetch methods as an array of objects

---
+++ src/scripts/api/fetch-methods.js
@@ -0,0 +1,78 @@
+const token = localStorage.getItem("token");
+
+export const fetchMetdhods = {
+  registerUser: {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: "",
+  },
+  loginUser: {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: "",
+  },
+  updateAvatarOption: {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: "",
+  },
+  getProfile: {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  },
+  getListing: {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  },
+  getListings: {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  },
+  createEntry: {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: "",
+  },
+  updateEntry: {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: "",
+  },
+  deleteEntry: {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  },
+  sendBidMethod: {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: "",
+  },
+};
