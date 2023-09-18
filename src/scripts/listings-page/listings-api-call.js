2361132b6f5988b60133b3d9ebc93db6925831fe
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-18 22:50:10 +0200

    added some imports and error handling in catch block

--- src/scripts/listings-page/listings-api-call.js
+++ src/scripts/listings-page/listings-api-call.js
@@ -1,16 +1,21 @@
-import { allActiveListingsDesc } from "../api/api-urls.js";
+import { allActiveListingsDescUrl } from "../api/api-urls.js";
 import { displayListingsUnlogged } from "../components/displayListingsUnlogged.js";
+import { errorHtml } from "../components/error.js";
+const listingsContainer = document.querySelector("#listings-container");
 
 async function getUnloggedListings(url) {
   try {
     const response = await fetch(url);
     const listings = await response.json();
-    console.log(listings);
+    // console.log(listings);
 
     displayListingsUnlogged(listings);
   } catch (error) {
     console.log(error);
+    listingsContainer.innerHTML = errorHtml(
+      "An error occured. Please wait a while and try again."
+    );
   }
 }
 
-getUnloggedListings(allActiveListingsDesc);
+getUnloggedListings(allActiveListingsDescUrl);
