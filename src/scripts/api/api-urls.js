ca329c767c335b9b49c41c671555b138df812220
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-18 22:46:47 +0200

    corrected the names of the urls´

--- src/scripts/api/api-urls.js
+++ src/scripts/api/api-urls.js
@@ -2,10 +2,13 @@
 export const api_base_url = `https://api.noroff.dev/api/v1`;
 
 // 4 latest listings from the index page, including created by a registered user
-export const latestListings = `${api_base_url}/auction/listings?sort=created&sortOrder=desc&limit=4&_active=true`;
+export const latestListingsUrl = `${api_base_url}/auction/listings?sort=created&sortOrder=desc&limit=4&_active=true`;
 
 // all active listings descending, including created by a registered user
-export const allActiveListingsDesc = `${api_base_url}/auction/listings?sort=created&sortOrder=desc&_active=true`;
+export const allActiveListingsDescUrl = `${api_base_url}/auction/listings?sort=created&sortOrder=desc&_active=true`;
 
 // all active listings, no sort order, including created by a registered user
-export const allActiveListings = `${api_base_url}/auction/listings?sort=created&_active=true`;
+export const allActiveListingsUrl = `${api_base_url}/auction/listings?sort=created&_active=true`;
+
+// register user
+export const registerUserUrl = `${api_base_url}/auction/auth/register`;
