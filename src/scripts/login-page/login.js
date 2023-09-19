49ecb9c08d2216203bc5630dd55a8f4613fb8e1b
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-19 13:33:50 +0200

    created the function

---
+++ src/scripts/login-page/login.js
@@ -0,0 +1,18 @@
+import { clearLocalStorage } from "../components/clear-storage.js";
+clearLocalStorage();
+
+import { loginUserUrl } from "../api//api-urls.js";
+import { loginAuthorizedUser } from "./login-api-call.js";
+
+const loginForm = document.querySelector("#login-form");
+
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // create user object
+  const myFormData = new FormData(event.target);
+  const userToLoginObject = Object.fromEntries(myFormData.entries());
+
+  // logs in to api
+  loginAuthorizedUser(loginUserUrl, userToLoginObject);
+});
