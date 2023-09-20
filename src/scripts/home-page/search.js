0cf512c2e0b33418b10f4b5d34c8113259115e9c
Karolina Szymanska <karolinaszymanska899@gmail.com>
2023-09-20 03:45:22 +0200

    .

--- src/scripts/home-page/search.js
+++ src/scripts/home-page/search.js
@@ -1,17 +1,29 @@
-import { allActiveListingsDescUrl } from "../api/api-base-urls.js";
-import { fetchMetdhods } from "../api/fetch-methods.js";
-const { getListing } = fetchMetdhods;
-import { displayListingsHome } from "./display-listings.js";
-import { searchListingsHome } from "./search.js";
+const searchInput = document.querySelector("#search-input");
+const statusMessageContainer = document.querySelector("#status-container");
 
-export async function fetchListings(url, method) {
-  try {
-    const response = await fetch(url, getListing);
-    const allListings = await response.json();
-    displayListingsHome(allListings);
-    searchListingsHome(allListings);
-  } catch (error) {
-    console.log(error);
-  }
+import { displayListingsHome } from "./display-listings.js";
+
+export function searchListingsHome(allListings) {
+  searchInput.addEventListener("input", (event) => {
+    let value = event.target.value;
+    let filteredArray = [];
+
+    for (let i = 0; i < allListings.length; i++) {
+      let currentPost = allListings[i];
+
+      if (
+        currentPost.title.toLowerCase().includes(value.toLowerCase()) ||
+        currentPost.description.toLowerCase().includes(value.toLowerCase())
+      ) {
+        filteredArray.push(currentPost);
+        statusMessageContainer.innerHTML = ``;
+      } else if (filteredArray.length === 0) {
+        statusMessageContainer.style.display = "block";
+        statusMessageContainer.innerHTML = `
+            <p class="">Oops! No items match the search terms</p>
+              `;
+      }
+    }
+    displayListingsHome(filteredArray);
+  });
 }
-fetchListings(allActiveListingsDescUrl);
